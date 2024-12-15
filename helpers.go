57e916130369f368b21faa11dffd7a187e7406ba
areon546 <57325743+areon546@users.noreply.github.com>
2024-12-15 13:50:13 +0000

    Update helpers.go

--- helpers.go
+++ helpers.go
@@ -10,20 +10,16 @@ import (
 
 // helper functions
 
-func print(a ...any) { fmt.Println(a...) }
+func Print(a ...any) { fmt.Println(a...) }
 
-func printf(s string, a ...any) { print(format(s, a...)) }
+func Printf(s string, a ...any) { print(format(s, a...)) }
 
-func format(s string, a ...any) string { return fmt.Sprintf(s, a...) }
+func Format(s string, a ...any) string { return fmt.Sprintf(s, a...) }
 
-func checkError(err error) {
+func CheckError(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-func assetsCSVPath() string {
-	return "assets.csv"
 }
 
 func search[T any](item T, arr []T) (index int) {
