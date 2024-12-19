3f2e386b5047d6204f05c22d8bc80824e534cadb
areon546 <57325743+areon546@users.noreply.github.com>
2024-12-19 12:32:49 +0000

    Added tests and testing helpers

--- helpers.go
+++ helpers.go
@@ -1,10 +1,12 @@
 package helpers
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"reflect"
 	"strconv"
+	"testing"
 )
 
 // helper functions
@@ -37,4 +39,42 @@ func ConvertToInteger(s string) (int, error) {
 		return i, err
 	}
 	return i, err
+}
+func AssertEquals(t testing.TB, expected, result fmt.Stringer) {
+	t.Helper()
+	if reflect.DeepEqual(expected, result) {
+		return
+	}
+
+	t.Log(expected.String(), result.String())
+
+	t.Errorf("Variables are not equal, \nexpected: %s \nresult: %s", expected, result)
+}
+
+func AssertEqualsInt(t testing.TB, expected, result int) {
+	t.Helper()
+
+	if expected == result {
+		return
+	}
+
+	t.Log(expected, result)
+
+	t.Errorf("Integers are not equal. \nexpected: %d \nresult: %d", expected, result)
+
+}
+
+func AssertError(t testing.TB, got, want error) {
+	t.Helper()
+
+	// log.Fatalf("\nexpected %q \ngot %q", want, got)
+	if !errors.Is(got, want) {
+		t.Fatalf("got error %q want %q", got, want)
+	}
+
+}
+
+func AssertNoError(t testing.TB, err error) {
+	t.Helper()
+	AssertError(t, err, nil)
 }
---
+++ helpers_test.go
@@ -0,0 +1,46 @@
+package helpers_test
+
+import (
+	"strconv"
+	"testing"
+
+	"github.com/areon546/go-helpers"
+)
+
+func TestSearch(t *testing.T) {
+	arr := []int{0, 1, 2}
+
+	t.Run("Search when value missing", func(t *testing.T) {
+		want := -1
+		get := helpers.Search(35, arr)
+
+		helpers.AssertEqualsInt(t, want, get)
+	})
+
+	t.Run("Search when value in arr", func(t *testing.T) {
+		helpers.AssertEqualsInt(t, 1, helpers.Search(1, arr))
+	})
+}
+
+func TestConvertToInteger(t *testing.T) {
+	t.Run("Convert Valid String to Integer", func(t *testing.T) {
+		want := 1
+		get, err := helpers.ConvertToInteger("1")
+
+		helpers.AssertNoError(t, err)
+		helpers.AssertEqualsInt(t, want, get)
+	})
+
+	t.Run("Convert Float String to Integer", func(t *testing.T) {
+		want := strconv.ErrSyntax
+		_, got := helpers.ConvertToInteger("1.0")
+
+		helpers.AssertError(t, got, want)
+	})
+	t.Run("Convert Invalid String to Integer", func(t *testing.T) {
+		want := strconv.ErrSyntax
+		_, got := helpers.ConvertToInteger("Abba")
+
+		helpers.AssertError(t, got, want)
+	})
+}
